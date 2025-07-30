93bbf57ea6d7e108fc641d810edd982e76b10229
Danh Tran <cprmmb99@gmail.com>
2025-07-30 15:38:41 +0700

    Update download_yolo_face_detection.py

--- utils/download_yolo_face_detection.py
+++ utils/download_yolo_face_detection.py
@@ -10,7 +10,7 @@ def download_yolo_face_detection_model():
     print("Downloading YOLOv11 face detection model...")
     # Define the checkpoint directory
     checkpoint_dir = "./ckpts"
-    yolo_dir = os.path.join(checkpoint_dir, "yolo11_face_detection")
+    yolo_dir = os.path.join(checkpoint_dir, "yolo_face_detection")
     # Create the checkpoint directory if it doesn't exist
     os.makedirs(yolo_dir, exist_ok=True)
 
