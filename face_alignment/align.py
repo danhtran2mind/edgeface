1bce64d84b26af8ecc98ec6066c25ba3f90b5d90
Danh Tran <cprmmb99@gmail.com>
2025-07-30 15:37:23 +0700

    Update align.py

--- face_alignment/align.py
+++ face_alignment/align.py
@@ -45,12 +45,12 @@ def get_aligned_face(image_path_or_image_paths, rgb_pil_image=None, algorithm='m
         if isinstance(image_path_or_image_paths, list):
             image_paths = image_path_or_image_paths
             results = face_yolo_detection(image_paths,
-                        # yolo_model_path="ckpts/yolo11_face_detection/model.pt",
+                        # yolo_model_path="ckpts/yolo_face_detection/model.pt",
                         use_batch=True, device=DEVICE)
         elif isinstance(image_path_or_image_paths, str):
             image_paths = [image_path_or_image_paths]
             results = face_yolo_detection(image_paths,
-                        # yolo_model_path="ckpts/yolo11_face_detection/model.pt",
+                        # yolo_model_path="ckpts/yolo_face_detection/model.pt",
                         use_batch=True, device=DEVICE)
         else:
             raise TypeError("image_path_or_image_paths must be a list or string") 
